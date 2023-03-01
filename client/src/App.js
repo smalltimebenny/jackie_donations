9876c423fbae70bb92c5d14186409b114a5fa763
smalltimebenny <ben.lindquist@gmail.com>
2023-02-28 19:58:49 -0500

    made it a react app to follow instructions i have

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Jackie from './components/Jackie';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Jackie />
+    </div>
+  );
+}
+
+export default App;
